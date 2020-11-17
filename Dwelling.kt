b4482a2430232db043ade544305add736ca7da14
Rushikesh Udhoji <rushikeshudhoji880@gmail.com>
2020-11-17 11:19:55 +0530

    Adding GitIgnore

---
+++ Dwelling.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+}
+class Dwelling(private var residents: Int){
+  abstract val buildingMaterial: String
+  abstract val capacity:Int
+
+  fun hasRoom():Boolean{
+    return residents < capacity
+  }
+}
